a372866b9fea76ed2caa180c04ebefcdf439362c
Gleb Parfinenko <71174262+Armgordon@users.noreply.github.com>
2022-04-01 10:14:25 +0800

    init commit

---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './AnswersList.module.scss'
+
+const AnswersList = () => {
+    return (
+        <ul className={}>
+
+        </ul>
+    );
+};
+
+export default AnswersList;
