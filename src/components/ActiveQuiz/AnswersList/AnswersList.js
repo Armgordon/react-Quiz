d5e1db443ed57aea4b4934114c8c25626c5c4a5e
Gleb Parfinenko <71174262+Armgordon@users.noreply.github.com>
2022-04-01 16:59:20 +0800

    Add: base functionality
    - add validation of answer;
    - add result-list (state of answers);
    - add UI components (button);
    - add drawer (open/close state, links);
    - add backdrop (close on click);

--- src/components/ActiveQuiz/AnswersList/AnswersList.js
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import classes from './AnswersList.module.scss'
+import AnswerItem from "./AnswerItem/AnswerItem";
 
-const AnswersList = () => {
+
+const AnswersList = (props) => {
     return (
-        <ul className={}>
+        <ul className={classes.AnswersList}>
+            {props.answers.map((answer, index)=> {
+                return(
+                    <AnswerItem
+                        key={index}
+                        answer={answer}
+                        onAnswerClick = {props.onAnswerClick}
+                        state={props.state ? props.state[answer.id] : null}
+                    />
+                )
+            })}
 
         </ul>
     );
